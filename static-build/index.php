e535e7055bb55aaa0a38e499d0f82fd30936ebb8
Edd Smith <edd@bigbrandideas.co.uk>
2019-04-29 11:55:57 +0100

    made some styling tweaks // needs to be compiled

--- static-build/index.php
+++ static-build/index.php
@@ -13,85 +13,72 @@
 
         <img class="highlight__billboard js-highlight-billboard">
 
-        <div class="overlay">
+        <nav class="overlay">
             <a class="overlay__text overlay__text--top-left" href="#contact">Say hello</a>
             <a class="overlay__text overlay__text--top-right" href="#about">About</a>
             <a class="overlay__text overlay__text--bottom-left" href="#">EddSmith.com</a>
             <a class="overlay__text overlay__text--bottom-right" href="#work">Work</a>
-        </div>
+        </nav>
 
-<!--         <header class="header">
-            <div class="container">
-                
-                <header class="header__wrap">
-                    <span class="header__ghost"></span>
-                    
-                    <h1 class="header__me">
-                        Edd Smith
-                    </h1>
-
-                    <h2 class="header__body">
-                        Web developer
-                    </h2>
-                </header>
-
-            </div>
-        </header> -->
-
-        <section class="about" id="about">
+        <header class="about" id="about">
 
             <div class="about__container">
 
-                <div class="about__content">
+                <article class="about__content">
             
-                    <p><span class="highlight js-highlight" data-highlight-src="assets/dist/images/what-up.gif">What&nbsp;up!</span> I'm Edd. I am a front end developer based in&nbsp;Derbyshire.</p>
+                    <h2><span class="highlight js-highlight" data-highlight-src="assets/dist/images/what-up.gif">What&nbsp;up!</span> I'm Edd. I am a front end developer based in&nbsp;Derbyshire.</h2>
 
-                    <p>I'm currently working at BigBrandIdeas, but you can look at some projects I've been working on below.</p>
+                    <h2>I'm currently working at BigBrandIdeas, but you can look at some projects I've been working on below.</h2>
 
-                    <p>I also write every now and again about anything that comes into my mind. If you feel like reading some pretty mediocre writing about completely meaningless topics - check out my blog.</p>
+                    <h2>I also write every now and again about anything that comes into my mind. If you feel like reading some pretty mediocre writing about completely meaningless topics - check out my blog.</h2>
 
-                </div>
+                </article>
 
             </div>
 
-        </section>
+        </header>
 
-        <section class="work" id="work">
-            
-            <div class="container">
+        <main>
+
+            <section class="work" id="work">
                 
-                <div class="work__wrap">
-
-                    <div class="work__inner js-work-archive no-js">
+                <div class="work__container">
                     
-                        <a class="work__item">
-                            <h3>Example Work Item One</h3>
-                        </a>
+                    <div class="work__wrap">
 
-                        <a class="work__item">
-                            <h3>Example Work Item One</h3>
-                        </a>
+                        <div class="work__inner js-work-archive no-js">
+                        
+                            <a class="work__item">
+                                <h3>Example Work Item One</h3>
+                            </a>
 
-                        <a class="work__item">
-                            <h3>Example Work Item One</h3>
-                        </a>
+                            <a class="work__item">
+                                <h3>Example Work Item One</h3>
+                            </a>
 
-                        <a class="work__item">
-                            <h3>Example Work Item One</h3>
-                        </a>
+                            <a class="work__item">
+                                <h3>Example Work Item One</h3>
+                            </a>
+
+                            <a class="work__item">
+                                <h3>Example Work Item One</h3>
+                            </a>
+
+                        </div>
+
+                        <div class="work__decoration">
+                            <img src="http://via.placeholder.com/413x600.png">
+                        </div>
 
                     </div>
-
-                    <div class="work__decoration">
-                        <img src="http://via.placeholder.com/413x600.png">
-                    </div>
-
 
                 </div>
 
-            </div>
+            </section>
 
-        </section>
+        </main>
+
+        <footer></footer>
 
     </body>
 
