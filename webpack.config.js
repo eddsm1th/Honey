e247eb47a608625609a4c2ae6db52903f1d2715e
Edd Smith <edd@bigbrandideas.co.uk>
2019-05-03 17:05:23 +0100

    made changes to config files to compile JS - but its not working :/

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 
-const 	path = require('path'),
+const 	workingStatic = true;
+
+		path = require('path'),
 		MiniCssExtractPlugin = require("mini-css-extract-plugin"),
 		OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin"),
 		TerserPlugin = require('terser-webpack-plugin'),
-		
-		outputPath = './static-build/assets/dist/css';
+
+		outputPath = './static-build/assets/dist/js';
 		devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
 
-	entry: { 
-		main: ['./static-build/assets/src/scss/main.scss']
+	entry: {
+		main: ['./static-build/assets/src/js/index.js', './static-build/assets/src/scss/main.scss']
 	},
 
 	output: {
 		path: path.resolve(__dirname, outputPath),
-		filename: 'main.css',
+		filename: 'main.js',
 		publicPath: '/'
 	},
 
 	optimization: {
 		minimizer: devMode ? [
+			new TerserPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: true
+			}),
 			new OptimizeCSSAssetsPlugin({})
 		] : []
 	},
@@ -29,6 +36,16 @@ module.exports = {
 
 	module: {
 		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ['babel-preset-env']
+					}
+				}
+			},
 
 			{
 		        test: /\.(sa|sc|c)ss$/,
@@ -58,7 +75,6 @@ module.exports = {
 	},
 
 	plugins: [
-
 	    new MiniCssExtractPlugin({
 	    	filename: devMode ? '../css/[name].css' : '../css/[name].[hash].css',
 	    	chunkFilename: devMode ? '../css/[id].css' : '../css/[id].[hash].css',
